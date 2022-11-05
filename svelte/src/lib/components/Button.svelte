2dfb35b68777d02bd174f17e748537cca4063e61
4kord <57727313+4kord@users.noreply.github.com>
2022-11-05 19:36:55 +0300

    add welcome

---
+++ svelte/src/lib/components/Button.svelte
@@ -0,0 +1,24 @@
+<button>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+        background-color: #682cd4;
+        color: inherit;
+        border: none;
+        border-radius: 5px;
+        padding: 10px;
+        font: inherit;
+    }
+    button:hover {
+        cursor: pointer;
+        background-color: #9074f4;
+    }
+    button:active {
+        border: 3px solid #4c3b81;
+        padding: 7px;
+        background-color: #271352;
+        cursor: pointer;
+    }
+</style>
---
+++ svelte/src/routes/Welcome.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Button from "$lib/components/Button.svelte";
+</script>
+
+<main>
+    <div class="content">
+        <h1>English Flashcards</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus impedit, soluta quaerat, quam officiis quibusdam libero vitae obcaecati id perspiciatis praesentium eaque necessitatibus aspernatur sunt reiciendis tempora! Iste, dolor similique?</p>
+        <Button>Login</Button>
+    </div>
+</main>
+
+<style>
+    h1, p {
+        text-align: left;
+    }
+    main {
+        padding: 0 100px;
+        height:700px;
+        background-color: #281454;
+    }
+    .content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 25px;
+        max-width: 400px;
+    }
+
+    @media screen and (max-width: 600px) {
+        main {
+            padding: 0 20px;
+        }
+    }
+</style>
